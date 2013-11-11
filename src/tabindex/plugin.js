bc683d199df5cb04d56fdb6009b2e1839a2532de
Andy Keller <pulazzo@gmail.com>
2013-11-11 08:52:10 -0500

    Syntax tweaks to build cleanly when installed into TinyMCE source.

--- src/tabindex/plugin.js
+++ src/tabindex/plugin.js
@@ -17,13 +17,17 @@
 //
 ///////////////////////////////////////////////////////////////////////////
 
-tinymce.PluginManager.add('tabindex', function(editor, url) {
+/*global tinymce:true */
 
-	editor.on('PostRender', function() { 
+tinymce.PluginManager.add('tabindex', function(editor) {
+
+	editor.on('PostRender', function() {
 		var iframe, tabindex = editor.settings.tabindex || 'element';
 		
 		// -1 => don't set a tabindex
-		if (tabindex === -1) return;
+		if (tabindex === -1) {
+                        return;
+                }
 
 		// 'element' => grab the tabindex from the element
 		if (tabindex === 'element') {
@@ -31,13 +35,17 @@ tinymce.PluginManager.add('tabindex', function(editor, url) {
 		}
 		
 		// make sure we have a tabindex
-		if (!tabindex) return;
+		if (!tabindex) {
+                        return;
+                }
 		
 		// get the iframe so we can set the tabindex
 		iframe = document.getElementById(editor.id + "_ifr");
 		
 		// make sure we have an iframe
-		if (!iframe) return;
+		if (!iframe) {
+                        return;
+                }
 
 		editor.dom.setAttrib(iframe, 'tabindex', tabindex);
 	});
